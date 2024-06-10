4dc55208f6409c82b875be8fdce12438d09a6637
fedi-nabli <fedinabli@gmail.com>
2024-06-10 04:53:54 +0100

    Launching the executable

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <unistd.h>
+#include <sys/ptrace.h>
 
-int main()
+int main(int argc, char* argv[])
 {
-  std::cout << "Hello World" << std::endl;
+  if (argc < 2)
+  {
+    std::cerr << "Program name not specified" << std::endl;
+    return -1;
+  }
+
+  auto prog = argv[1];
+
+  auto pid = fork();
+  if (pid == 0)
+  {
+    // We're in the child process
+    // execute debugee
+    ptrace(PTRACE_TRACEME, 0, nullptr, nullptr);
+    execl(prog, prog, nullptr);
+  }
+  else if (pid >= 1)
+  {
+    // We're in the parent process
+    // execute debugger
+  }
 
   return 0;
 }
